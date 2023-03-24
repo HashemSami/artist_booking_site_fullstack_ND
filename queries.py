389fd63235c7ff760e723abc9afff85fc408e450
hashemsami <hashem.sf@gmail.com>
2023-03-25 01:08:41 +0300

    first commit

---
+++ queries.py
@@ -0,0 +1,31 @@
+from app import (
+    db,
+    Venue,
+    Artist,
+    Genre,
+    Show,
+    app,
+    genres_venues,
+    genres_artists,
+)
+import datetime
+
+app.app_context().push()
+
+print(Genre.query.all())
+
+# CA = City(name="San Francisco", state="CA")
+# NY = City(name="New York", state="NY")
+
+# db.session.add_all([CA, NY])
+
+# db.drop_all()
+db.session.query(genres_venues).delete()
+db.session.query(genres_artists).delete()
+Show.query.delete()
+Venue.query.delete()
+Artist.query.delete()
+Genre.query.delete()
+
+db.session.commit()
+db.session.close()
--- static/js/script.js
+++ static/js/script.js
@@ -2,3 +2,18 @@ window.parseISOString = function parseISOString(s) {
   var b = s.split(/\D+/);
   return new Date(Date.UTC(b[0], --b[1], b[2], b[3], b[4], b[5], b[6]));
 };
+
+// const delete_button = document.getElementById("delete_button");
+
+// delete_button.addEventListener("click", async e => {
+//   console.log(e);
+//   const id = e.target.dataset.id;
+
+//   await fetch("/venues/" + id, {
+//     method: "DELETE",
+//     headers: {
+//       "Content-Type": "application/json",
+//       // 'Content-Type': 'application/x-www-form-urlencoded',
+//     },
+//   });
+// });
